1e279eb3e8a03efe491ad8da553750e60f50d504
Isobel Beasley <isobel.beasley@ucsf.edu>
2025-02-12 13:55:52 -0800

    Initialise NW algorithm in unit tests

--- test/test_align.py
+++ test/test_align.py
@@ -14,6 +14,17 @@ def test_nw_alignment():
     """
     seq1, _ = read_fasta("./data/test_seq1.fa")
     seq2, _ = read_fasta("./data/test_seq2.fa")
+    
+    # Initalise NW algorithm
+    # Using the BLOSUM62 matrix and a gap open penalty
+    # of -10 and a gap extension penalty of -1.
+    NW_alg = NeedlemanWunsch(sub_matrix_file = 'substitution_matrices/BLOSUM62.mat',
+                             gap_open = -10,
+                             gap_extend = -1
+                            )
+                            
+    align_score, seq1_align, seq2_align = NW_alg.align(seq1, seq2)                    
+    
     pass
     
 
@@ -27,6 +38,22 @@ def test_nw_backtrace():
     """
     seq3, _ = read_fasta("./data/test_seq3.fa")
     seq4, _ = read_fasta("./data/test_seq4.fa")
+    
+    # Initalise NW algorithm
+    # Using the BLOSUM62 matrix and a gap open penalty
+    # of -10 and a gap extension penalty of -1.
+    NW_alg = NeedlemanWunsch(sub_matrix_file = 'substitution_matrices/BLOSUM62.mat',
+                             gap_open = -10,
+                             gap_extend = -1
+                            )
+                            
+    align_score, seq3_align, seq4_align = NW_alg.align(seq3, seq4)  
+    
+    # print(align_score)
+    # print(seq3_align)
+    # print(seq4_align)
+    # raise ValueError("????")
+   
     pass
 
 
