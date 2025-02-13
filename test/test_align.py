38cb4855ecd258895a84dcb58548c96f3cac3baa
Isobel Beasley <isobel.beasley@ucsf.edu>
2025-02-12 22:51:34 -0800

    Trying to complete unit tests for alignment matrices

--- test/test_align.py
+++ test/test_align.py
@@ -25,11 +25,48 @@ def test_nw_alignment():
                             
     align_score, seq1_align, seq2_align = NW_alg.align(seq1, seq2)
     
-    # print(align_score)
-    # print(NW_alg._align_matrix)
-    # print(NW_alg._align_matrix.all())
+    # 1. Check align_matrix: 
     
-    pass
+    # align matrix should look like this: 
+    #[[  0. -inf -inf -inf]
+    # [-inf   5. -11. -13.]
+    # [-inf -12.   4.  -8.]
+    # [-inf -12.  -1.   5.]
+    # [-inf -14.  -6.   4.]]
+   
+    true_align_mat = np.array([
+                                [0,   -float('inf'), -float('inf'), -float('inf')],
+                                [-float('inf'),   5,  -11,  -13], #????13],
+                                [-float('inf'), -12,    4,   -8],
+                                [-float('inf'), -12,   -1,    5],
+                                [-float('inf'), -14,   -6,    4]
+                              ])
+    
+    
+    print(NW_alg._align_matrix)
+    
+    assert np.allclose(true_align_mat, NW_alg._align_matrix), "Error: Estimated alignment matrix is incorrect (for seq1 and seq2)" 
+
+
+    # 2. Check gapA_matrix
+    # gapA_matrix should look like this: 
+    #[[-10 -inf. -inf. -inf.]
+    # [-11 -inf. -inf. -inf.]
+    # [-12  ?.  ?.  ?.]
+    # [-13 -?.  ?.  ?.]
+    # [-14 -?.  ?.  ?.]]
+    print(NW_alg._gapA_matrix)
+    
+    # 3. Check gapB_matrix
+    # gapA_matrix should look like this: 
+    #[[-10 -11. -12. -13.]
+    # [-inf  -inf. ?. ?.]
+    # [-inf  -inf.  ?.  ?.]
+    # [-inf -inf.  ?.  ?.]
+    # [-inf -inf.  ?.  ?.]]
+    print(NW_alg._gapB_matrix)
+    
+    raise ValueError("ello")
     
 
 def test_nw_backtrace():
@@ -53,9 +90,13 @@ def test_nw_backtrace():
                             
     align_score, seq3_align, seq4_align = NW_alg.align(seq3, seq4)  
     
-    assert seq3_align == 'MAVHQLIRRP', 'seq3 error'
-    assert seq4_align == 'M---QLIRHP', 'seq4 error'
-    assert align_score == 17, 'score error'
+    # Check alignment score is correct: 
+    assert align_score == 17, 'Error in NW backtracking: alignment score was not calculated corretly'
+    
+    # Check that aligned sequences are correct:
+    assert seq3_align == 'MAVHQLIRRP', 'Error in NW backtracking: sequence was not aligned correctly (seq3)'
+    assert seq4_align == 'M---QLIRHP', 'Error in NW backtracking: sequence was not aligned correctly (seq4)'
+   
     
 
 
